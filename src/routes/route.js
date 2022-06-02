e63d6565758e9cb2acf4368ebc23578ee6d5f10b
functionup-demo <functionup.demo@gmail.com>
2022-06-02 13:19:29 +0530

    query and path params intro

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const myHelper = require('../util/helper')
+const underscore = require('underscore')
 
 const router = express.Router();
 
@@ -7,8 +8,33 @@ router.get('/test-me', function (req, res) {
     myHelper.printDate()
     myHelper.getCurrentMonth()
     myHelper.getCohortData()
+    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
+    console.log('The first element received from underscope function is '+firstElement)
     res.send('My first ever api!')
 });
+
+router.get('/hello', function (req, res) {
+   
+    res.send('Hello there!')
+});
+
+router.get('/candidates', function(req, res){
+    console.log('Query paramters for this request are '+JSON.stringify(req.query))
+    let gender = req.query.gender
+    let state = req.query.state
+    let district = req.query.district
+    console.log('State is '+state)
+    console.log('Gender is '+gender)
+    console.log('District is '+district)
+    let candidates = ['Akash','Suman']
+    res.send(candidates)
+})
+
+router.get('/candidates/:canidatesName', function(req, res){
+    console.log('The request objects is '+ JSON.stringify(req.params))
+    console.log('Candidates name is '+req.params.canidatesName)
+    res.send('Done')
+})
 
 
 module.exports = router;
