aa64937607723e6584123a4de31f121bc50a7c49
ravikrpandey <104829126+ravikrpandey@users.noreply.github.com>
2022-06-02 06:40:16 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const externalModule = require('../logger/logger')
+const module2=require('../util/helper')
+const module3=require('../validator/formatter.js')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    externalModule.welcome()
+    module2.PrintDate()
+    module2.PrintMonth()
+    module2.getBatchInfo()
+    module3.spaceTrim()
+    module3.toLowerCh()
+    module3.toUpperCh()
+    res.send('Welcome to my application. I am Ravi Kumar pandey and a part of FunctionUp Radon cohort')
 });
+
+
+
+
+
 
 module.exports = router;
 // adding this comment for no reason
