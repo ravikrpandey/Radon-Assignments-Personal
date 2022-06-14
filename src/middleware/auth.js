326e0f702753563edc11223457f5c2fbb709d6c0
ravikrpandey <104829126+ravikrpandey@users.noreply.github.com>
2022-06-15 00:41:29 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,20 +1,20 @@
-const jwt = require("jsonwebtoken");
-
-let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FunctionUp",
-    },
-    "functionup-radon"
-  );
-  let decodedToken = jwt.verify(token, "functionup-radon");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
-
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
-
-  res.send({ status: true, data: userDetails });
+const userModel = require("../models/userModel");
+ const authLoginUser= async function(req, res, next){
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.send({
+        status: false,
+        msg: "username or the password is not corerct"
+      }) 
+      next()
+ }
+ const authToken= async function(req, res, next){
+    let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+  if (!token) return res.send({ status: false, msg: "token must be present" })
+  next()
+ }
+ module.exports.authToken= authToken
+ module.exports.authLoginUser= authLoginUser
